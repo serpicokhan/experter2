f9299d02f4962641dbedc01b5d476bc13cabe669
moeinserpico <morteza20413100@gmail.com>
2023-07-31 14:10:36 +0330

    dsa
    dsadsa

--- cmms/lib/pages/workorderform.dart
+++ cmms/lib/pages/workorderform.dart
@@ -1,3 +1,4 @@
+import 'package:cmms/main.dart';
 import 'package:cmms/util/util.dart';
 import 'package:flutter/material.dart';
 import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
@@ -134,7 +135,8 @@ class _FormScreenState extends State<FormScreen> {
       // For example, show an error message.
       _showSnackBar(context, error.toString());
     }
-    Navigator.pop(context);
+    Navigator.push(
+        context, MaterialPageRoute(builder: (context) => BottomNavigation()));
   }
 }
 
